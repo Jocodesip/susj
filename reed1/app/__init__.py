f471426f4f431dcaf0368ad95fe520cb606a7494
Jocodesip <josip.milkovic12@gmail.com>
2024-06-29 10:48:04 +0200

    Konfiguracija baze podataka

--- reed1/app/__init__.py
+++ reed1/app/__init__.py
@@ -1,7 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from app.routes import main
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///reeds.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
     app.register_blueprint(main)
+
+    with app.app_context():
+        db.create_all()
+        
     return app
