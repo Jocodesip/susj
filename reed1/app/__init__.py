7cefe265dc6d3421ce4880edd439200c80273ba8
Jocodesip <josip.milkovic12@gmail.com>
2024-06-29 11:24:16 +0200

    1

--- reed1/app/__init__.py
+++ reed1/app/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///reeds.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/reeds.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
